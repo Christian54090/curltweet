5e1dba0884100ffab2753de3103314b05258185d
Christian Sanders <christian.sanders@live.com>
2018-11-20 18:38:45 -0800

    Grabs video download url

---
+++ lib/curltweet.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+@tweet = ARGV[0]
+@token = @tweet.split('/')[5]
+@link  = "https://twitter.com/i/videos/tweet/#{@token}"
+
+def download_url
+  # Grab and parse video HTML. Search result for script src
+  doc = Nokogiri::HTML(open(@link))
+  src = doc.at_css("script").to_s.split('"')[1]
+  res = Nokogiri::HTML(open(src))
+
+  # Grab auth token
+  auth_token = res.to_s.match(/Bearer ([a-zA-Z0-9%-])+/).to_s
+
+  # Talk to API
+  api_link = "https://api.twitter.com/1.1/videos/tweet/config/#{@token}.json"
+  api = open(api_link, 'Authorization' => auth_token)
+  video_url = JSON.load(api)
+  video_url = video_url['track']['playbackUrl']
+
+  puts video_url
+  # Grab video file
+#  video_res =
+end
+
+download_url
+
+# %x!/usr/bin/env curl -L -C - -o #{@link} #{download_url}!
